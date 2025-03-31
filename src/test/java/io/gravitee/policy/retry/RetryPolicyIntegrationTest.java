c224f6caa843428497df3f5ece145c08540e8b83
Jedrzej Janasiak <jedrzej.janasiak@graviteesource.com>
2025-03-31 17:37:04 +0200

    chore(deps): bump gravitee dependencies to 4.6.7

--- src/test/java/io/gravitee/policy/retry/RetryPolicyIntegrationTest.java
+++ src/test/java/io/gravitee/policy/retry/RetryPolicyIntegrationTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,37 +15,38 @@
  */
 package io.gravitee.policy.retry;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
-import static com.github.tomakehurst.wiremock.client.WireMock.ok;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
 import io.gravitee.apim.gateway.tests.sdk.annotations.DeployApi;
 import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
+import io.gravitee.definition.model.ExecutionMode;
 import io.gravitee.policy.retry.configuration.RetryPolicyConfiguration;
-import io.reactivex.observers.TestObserver;
-import io.vertx.reactivex.core.buffer.Buffer;
-import io.vertx.reactivex.ext.web.client.HttpResponse;
-import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.rxjava3.core.http.HttpClient;
+import io.vertx.rxjava3.core.http.HttpClientRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Thibaud AVENIER (yann.tavernier at graviteesource.com)
  * @author GraviteeSource Team
  */
-@GatewayTest
+@GatewayTest(v2ExecutionMode = ExecutionMode.V3)
 @DeployApi("/apis/retry.json")
 class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPolicyConfiguration> {
 
     @Test
     @DisplayName("Should succeed before max retries")
-    void shouldSucceedBeforeMaxRetries(WebClient client) {
+    void shouldSucceedBeforeMaxRetries(HttpClient client) {
         wiremock.stubFor(
             WireMock
                 .get("/endpoint")
@@ -71,10 +72,12 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
                 .willReturn(ok())
         );
 
-        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test").rxSend().test();
-        awaitTerminalEvent(obs);
 
-        obs
+        client
+            .rxRequest(HttpMethod.GET, "/test")
+            .flatMap(HttpClientRequest::rxSend)
+            .test()
+            .awaitDone(10, SECONDS)
             .assertComplete()
             .assertValue(response -> {
                 assertThat(response.statusCode()).isEqualTo(200);
@@ -82,12 +85,13 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
             })
             .assertNoErrors();
 
+
         wiremock.verify(3, getRequestedFor(urlPathEqualTo("/endpoint")));
     }
 
     @Test
     @DisplayName("Should fail after max retries")
-    void shouldFailAfterMaxRetries(WebClient client) {
+    void shouldFailAfterMaxRetries(HttpClient client) {
         wiremock.stubFor(
             WireMock
                 .get("/endpoint")
@@ -116,10 +120,12 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
             WireMock.get("/endpoint").inScenario("retry").whenScenarioStateIs("thirdCall").willReturn(aResponse().withStatus(508))
         );
 
-        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test").rxSend().test();
-        awaitTerminalEvent(obs);
 
-        obs
+        client
+            .rxRequest(HttpMethod.GET, "/test")
+            .flatMap(HttpClientRequest::rxSend)
+            .test()
+            .awaitDone(10, SECONDS)
             .assertComplete()
             .assertValue(response -> {
                 assertThat(response.statusCode()).isEqualTo(502);
@@ -132,7 +138,7 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
 
     @Test
     @DisplayName("Should fail after all retries in timeout")
-    void shouldFailAfterTimeout(WebClient client) {
+    void shouldFailAfterTimeout(HttpClient client) {
         wiremock.stubFor(
             WireMock
                 .get("/endpoint")
@@ -161,10 +167,11 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
             WireMock.get("/endpoint").inScenario("retry").whenScenarioStateIs("thirdCall").willReturn(ok().withFixedDelay(600))
         );
 
-        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test").rxSend().test();
-        awaitTerminalEvent(obs);
-
-        obs
+        client
+            .rxRequest(HttpMethod.GET, "/test")
+            .flatMap(HttpClientRequest::rxSend)
+            .test()
+            .awaitDone(10, SECONDS)
             .assertComplete()
             .assertValue(response -> {
                 assertThat(response.statusCode()).isEqualTo(502);
@@ -178,7 +185,7 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
     @Test
     @DeployApi("/apis/retry-last-response.json")
     @DisplayName("Should fail after too many conditions failure and return last response")
-    void shouldFailAfterTooManyConditionFailureAndReturnLastResponse(WebClient client) {
+    void shouldFailAfterTooManyConditionFailureAndReturnLastResponse(HttpClient client) throws InterruptedException {
         wiremock.stubFor(
             WireMock
                 .get("/endpoint")
@@ -205,10 +212,12 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
         );
         wiremock.stubFor(WireMock.get("/endpoint").inScenario("retry").whenScenarioStateIs("thirdCall").willReturn(notFound()));
 
-        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test-last-response").rxSend().test();
-        awaitTerminalEvent(obs);
 
-        obs
+        client
+            .rxRequest(HttpMethod.GET, "/test-last-response")
+            .flatMap(HttpClientRequest::rxSend)
+            .test()
+            .awaitDone(10, SECONDS)
             .assertComplete()
             .assertValue(response -> {
                 assertThat(response.statusCode()).isEqualTo(404);
@@ -222,7 +231,7 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
     @Test
     @DeployApi("/apis/retry-last-response.json")
     @DisplayName("Should fail after too many timeout failures and does not return last response")
-    void shouldFailAfterTimeoutAndReturnLastResponse(WebClient client) {
+    void shouldFailAfterTimeoutAndReturnLastResponse(HttpClient client) throws InterruptedException {
         wiremock.stubFor(
             WireMock
                 .get("/endpoint")
@@ -251,13 +260,13 @@ class RetryPolicyIntegrationTest extends AbstractPolicyTest<RetryPolicy, RetryPo
             WireMock.get("/endpoint").inScenario("retry").whenScenarioStateIs("thirdCall").willReturn(ok().withFixedDelay(600))
         );
 
-        final TestObserver<HttpResponse<Buffer>> obs = client.get("/test-last-response").rxSend().test();
-        awaitTerminalEvent(obs);
-
-        obs
+        client
+            .rxRequest(HttpMethod.GET, "/test-last-response")
+            .flatMap(HttpClientRequest::rxSend)
+            .test()
+            .awaitDone(10, SECONDS)
             .assertComplete()
             .assertValue(response -> {
-                // In the case of a timeout, lastResponse option does not do anything
                 assertThat(response.statusCode()).isEqualTo(502);
                 return true;
             })
